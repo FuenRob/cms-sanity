20986dcfda5ab828e8cc8d4c266cdc2f39ab4532
FuenRob <fuenrob@gmail.com>
2021-01-10 13:19:22 +0100

    Finish home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
 import React from 'react'
-
+import image from '../images/home-img.jpg'
 export default function Home() {
-    return <h1>Home Page!</h1>;
+    return (
+        <main>
+            <img 
+                src={image} 
+                alt="Home img"
+                className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold leading-none lg:leading-snug home-name">Hello, i'm FuenRob</h1>
+            </section>
+        </main>
+    )
 }
