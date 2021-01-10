e106c9143bc131002b91f05e093c90386cb64546
FuenRob <fuenrob@gmail.com>
2021-01-10 12:57:22 +0100

    Finish the navbar component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import About from "./components/About"
 import Posts from "./components/Posts"
 import SinglePost from "./components/SinglePost"
 import Projects from "./components/Projects"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Switch>
         <Route component={ Home } path='/' exact />
         <Route component={ About } path='/about' />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,67 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { SocialIcon } from 'react-social-icons'
 
 export default function Navbar() {
-    return <h1>Navbar Page!</h1>;
+    return (
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink 
+                        to="/" 
+                        exact 
+                        activeClassName="text-white" 
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold tracking-widest"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink 
+                        to="/posts"
+                        activeClassName="text-red-100 bg-red-700"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    >
+                        Blog
+                    </NavLink>
+                    <NavLink 
+                        to="/projects"
+                        activeClassName="text-red-100 bg-red-700"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    >
+                        Projects
+                    </NavLink>
+                    <NavLink 
+                        to="/about"
+                        activeClassName="text-red-100 bg-red-700"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    >
+                        About
+                    </NavLink>
+                </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon 
+                        url="https://www.linkedin.com/in/robercmorais" 
+                        className="mr-4" 
+                        fgColor="#fff" 
+                        target="_blank" 
+                        style={{ height:35, width: 35 }} 
+                    />
+                    <SocialIcon 
+                        url="https://twitter.com/FuenRob" 
+                        className="mr-4" 
+                        fgColor="#fff" 
+                        target="_blank" 
+                        style={{ height:35, width: 35 }} 
+                    />
+                    <SocialIcon 
+                        url="https://www.fuenrob.com/" 
+                        label="My Blog" 
+                        className="mr-4" 
+                        fgColor="#fff" 
+                        target="_blank" 
+                        style={{ height:35, width: 35 }} 
+                    />
+                </div>
+            </div>
+        </header>
+    )
 }
